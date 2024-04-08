faf708f690274cae8480222841223a32b3b06b08
Shubham Padule <91106911+Shubham-Padule@users.noreply.github.com>
2024-04-08 12:03:54 +0530

    node

---
+++ node js/5-4-24/demo.js
@@ -0,0 +1,16 @@
+// case 1
+let x1=require("./myfile.js");
+
+console.log(x1);
+
+// // case2
+// let funadd=require("./myfile.js");
+// var sum=funadd(2,2);
+// console.log("sum=+sum"); 
+  
+// case3
+// let mathobj=require("./myfile.js");
+// var res=mathobj.add(1,2);
+// console.log("addition="+res);
+// var res=mathobj.sub(2,2);
+// console.log("difference="+res)
---
+++ node js/5-4-24/myfile.js
@@ -0,0 +1,23 @@
+// case1
+var x=100;
+module.exports=x;
+
+ 
+
+// // case2
+// var add=function(a,b){
+//     return a+b;
+// }
+// module.exports=add;
+
+// var obj = {
+
+//     add:function(a,b){
+//         return a+b;
+//     },
+//     sub:function(a,b){
+//         return a-b;
+//     }
+    
+// }
+// module.exports=obj;
